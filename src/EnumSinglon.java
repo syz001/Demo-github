9dbc7eac62eb50f0a95a7949cc753c49269fd5e4
sy001 <hero_sang@126.com>
2020-09-29 10:00:36 +0800

    开发a的修改第二次

--- src/EnumSinglon.java
+++ src/EnumSinglon.java
@@ -10,7 +10,7 @@ class TestEnumSingleton{
         // 直接调用静态方法获得，而不是使用new
         EnumSinglon singleton1 = EnumSinglon.INSTANCE;
         EnumSinglon singleton2 = EnumSinglon.INSTANCE;
-
+        //开发人员a做的修改
         // 发现两个对象相等
         System.out.println(singleton1 == singleton2); //返回true
 
